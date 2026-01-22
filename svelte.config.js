7140a5895715a0cd3238fac6d34a1dbc62155974
peterreeves <peter.x.reeves@gmail.com>
2026-01-22 11:43:16 +1100

    Further setup.

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,19 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
+    compilerOptions: {
+        runes: true,
+        experimental: {
+            async: true,
+        },
+    },
     kit: {
         adapter: adapter({
             fallback: "index.html",
         }),
+        experimental: {
+            remoteFunctions: true,
+        },
     },
 };
 
