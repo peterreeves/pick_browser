c67b7932869e01f43e3d2686ecdd8c5edfcf2237
peterreeves <peter.x.reeves@gmail.com>
2026-01-22 16:25:20 +1100

    Styling pass.

--- src/lib/components/BrowserList.svelte
+++ src/lib/components/BrowserList.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/core";
-    import { SvelteMap } from "svelte/reactivity";
     import type { Browser, BrowserIcon } from "./defs";
 
     type Props = {
@@ -10,23 +9,25 @@
     let { urlToOpen }: Props = $props();
 
     let openingBrowser = $state(false);
-    let iconCache = new SvelteMap<string, string>();
 
     let browsers = $derived(await invoke<Array<Browser>>("get_browsers"));
 
-    const loadIcon = async (browser: Browser) => {
-        if (!browser.icon) return;
-        if (iconCache.has(browser.id)) return;
+    const getBrowserIcon = async (browser: Browser): Promise<string | null> => {
+        if (browser.icon === null) {
+            return null;
+        }
 
-        const icon = await invoke<BrowserIcon | null>("get_browser_icon", { id: browser.id });
-        if (icon) {
-            iconCache.set(browser.id, `data:${icon.mime_type};base64,${icon.data}`);
+        try {
+            const icon = await invoke<BrowserIcon | null>("get_browser_icon", { id: browser.id });
+            if (icon !== null) {
+                return `data:${icon.mime_type};base64,${icon.data}`;
+            } else {
+                return null;
+            }
+        } catch {
+            return null;
         }
     };
-
-    $effect(() => {
-        browsers.forEach((browser) => loadIcon(browser));
-    });
 
     const openBrowser = async (id: string) => {
         try {
@@ -40,8 +41,9 @@
 
 <div class="browser-grid">
     {#each browsers as browser (browser.id)}
+        {@const browserIcon = await getBrowserIcon(browser)}
         <div class="browser">
-            <button>
+            <button class="browser-menu">
                 <!-- TODO: Add menu button to allow editing/removing this entry -->
             </button>
             <button
@@ -49,44 +51,69 @@
                 class="browser-icon"
                 disabled={openingBrowser}
             >
-                {#if iconCache.has(browser.id)}
-                    <img src={iconCache.get(browser.id)} alt={browser.name} class="icon-img" />
+                {#if browserIcon !== null}
+                    <img src={browserIcon} alt={browser.name} class="icon-img" />
                 {:else}
                     🌐
                 {/if}
+                <p class="browser-name">{browser.name}</p>
             </button>
-            <p>{browser.name}</p>
         </div>
     {/each}
     <div class="browser">
-        <a href="/new" class="browser-icon">➕</a>
-        <p>Add new</p>
+        <div class="browser-new">
+            <a href="/new">➕</a>
+            <p>Add new</p>
+        </div>
     </div>
 </div>
 
 <style>
     .browser-grid {
         display: grid;
-        grid-template-columns: repeat(4, 128px);
+        grid-template-columns: 1fr 1fr 1fr 1fr;
         gap: 8px;
         border-radius: 8px;
     }
 
     .browser {
-        border: 1px solid var(--zinc-500);
-        padding: 4px;
         aspect-ratio: 1;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        width: 128px;
+        height: 128px;
+        overflow: hidden;
     }
 
-    .browser-icon {
-        font-size: 2.5rem;
-        line-height: 1;
+    .browser-menu {
+        position: absolute;
+        top: 8px;
+        right: 8px;
+        width: 24px;
+        aspect-ratio: 1;
+        border-radius: 50%;
+    }
+
+    .browser-icon,
+    .browser-new {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
         text-decoration: none;
+        padding: 0px;
+        background-color: var(--zinc-200);
+        border: 1px solid var(--zinc-900);
+        border-radius: 16px;
+        overflow: hidden;
     }
 
     .icon-img {
-        width: 2.5rem;
-        height: 2.5rem;
         object-fit: contain;
+        flex: 1 1 auto;
+        height: 0px;
     }
 </style>
